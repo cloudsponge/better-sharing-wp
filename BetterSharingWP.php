0dbbce90f7ad290a9a8077ff5dd65051f5f5be49
Roy Sivan <roy@roysivan.com>
2019-11-10 23:27:56 -0800

    init commit

---
+++ BetterSharingWP.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * @wordpress-plugin
+ * Plugin Name:       Better Sharing WP
+ * Description:       Better Sharing WordPress plugin for use for CloudSponge
+ * Version:           1.0.0
+ * Author:            CloudSponge
+ * Author URI:        https://www.cloudsponge.com
+ * License:           GPL-3.0
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * Textdomain:        better-sharing-wp
+ */
+
+namespace BetterSharingWP;
+
+include_once 'vendor/autoload.php';
+
+use BetterSharingWP\AdminScreen;
+
+class BetterSharingWP {
+
+	private $adminScreen;
+
+	public function __construct() {
+		$adminScreen = new AdminScreen();
+	}
+
+
+}
+
+global $BetterSharingWP;
+
+$BetterSharingWP = new BetterSharingWP();
---
+++ includes/AdminScreen.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roysivan
+ * Date: 11/10/19
+ * Time: 11:20 PM
+ */
+
+namespace BetterSharingWP;
+
+
+class AdminScreen {
+
+	public function __construct() {
+		add_action( 'admin_menu', [ $this, 'better_sharing_menu_init' ], 10 );
+	}
+
+	public function better_sharing_menu_init() {
+		add_menu_page(
+			'Better Sharing WP',
+			'Better Sharing',
+			'manage_options',
+			'better-sharing-wp',
+			[ $this, 'better_sharing_menu' ],
+			'dashicons-megaphone'
+		);
+	}
+
+	public function better_sharing_menu() {
+		echo '<h2>Better Sharing WP</h2>';
+	}
+
+}
