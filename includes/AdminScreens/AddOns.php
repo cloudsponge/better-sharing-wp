b06ee7f0ebe0b88918300869a6b9d191d7cc8b1d
Brian Siklinski <bszyk.codes@gmail.com>
2020-12-22 09:43:50 -0500

    added sanitizing to $_POST requests

--- includes/AdminScreens/AddOns.php
+++ includes/AdminScreens/AddOns.php
@@ -70,12 +70,19 @@ class AddOns {
 		}
 
 		// Save Data.
-		$post = $_POST;
-		if ( ! isset( $_POST['__bswp_api_key__save'] ) ) {
+		$post = array();
+
+		if ( ! empty( $_POST ) && is_array( $_POST ) ) {
+				foreach ( $_POST as $key => $value ) {
+						$post[$key] = sanitize_text_field( $value );
+				}
+		}
+
+		if ( ! isset( $post['__bswp_api_key__save'] ) ) {
 			return;
 		}
-		if ( isset( $_POST['__bswp_api_key'] ) ) {
-			$api_key_saved = $this->save_api_key( sanitize_text_field( $_POST['__bswp_api_key'] ) );
+		if ( isset( $post['__bswp_api_key'] ) ) {
+			$api_key_saved = $this->save_api_key( $post['__bswp_api_key'] );
 			if ( is_wp_error( $api_key_saved ) ) {
 				$this->error_msg = $api_key_saved->get_error_message();
 				add_action(
