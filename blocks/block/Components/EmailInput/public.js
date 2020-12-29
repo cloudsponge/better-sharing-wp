f5448f6dfdc31e4a20a066a2a33ba375256c56c9
Brian Siklinski <bszyk.codes@gmail.com>
2020-12-29 16:37:23 -0500

    updated selectors

--- blocks/block/Components/EmailInput/public.js
+++ blocks/block/Components/EmailInput/public.js
@@ -1,21 +1,18 @@
 // create an array of emails
-class EmailInput {
+export default class EmailInput {
   constructor() {
-    this.emailInput = document.getElementById('email-input');
-    this.submit = document.getElementById('email-form-submit');
+    this.emailInput = document.getElementById('bswp-share-email-input');
+    this.submit = document.querySelector('.bswp-submit');
     this.events();
   }
 
   events() {
-    this.submit.addEventListener('click', () =>
-      console.log(this.handleEmails(this.emailInput.value))
-    );
+    this.submit.addEventListener('click', (e) => {
+      e.preventDefault();
+      console.log(this.handleEmails(this.emailInput.value));
+    });
   }
 
   // returns an array of emails from the input that were separated by a comma
   handleEmails = (emails) => emails.split(',').map((email) => email.trim());
 }
-
-document.addEventListener('DOMContentLoaded', function () {
-  const emailInput = new EmailInput();
-});
