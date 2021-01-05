ed0e1e0be1473eeb17ee66a96f4247324ec2a9bf
Brian Siklinski <bszyk.codes@gmail.com>
2021-01-05 18:05:41 -0500

    add nonce and $_POST sanitation fix

--- includes/AdminScreens/AddOns.php
+++ includes/AdminScreens/AddOns.php
@@ -69,30 +69,26 @@ class AddOns {
 			$this->option_data = $option_data;
 		}
 
-		// Save Data.
-		$post = array();
-
-		if ( ! empty( $_POST ) && is_array( $_POST ) ) {
-				foreach ( $_POST as $key => $value ) {
-						$post[$key] = sanitize_text_field( $value );
-				}
-		}
-
-		if ( ! isset( $post['__bswp_api_key__save'] ) ) {
+		if ( ! isset( $_POST['_bswp_settings_nonce'] ) || ! wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['_bswp_settings_nonce'] ) ), 'bswp_settings_nonce' ) ) {
 			return;
-		}
-		if ( isset( $post['__bswp_api_key'] ) ) {
-			$api_key_saved = $this->save_api_key( $post['__bswp_api_key'] );
-			if ( is_wp_error( $api_key_saved ) ) {
-				$this->error_msg = $api_key_saved->get_error_message();
-				add_action(
-					'admin_notices',
-					function () {
-						$class   = 'notice notice-error';
-						$message = $this->error_msg;
-						printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
-					}
-				);
+		} else {
+			// Save Data.
+			if ( ! isset( $_POST['__bswp_api_key__save'] ) ) {
+				return;
+			}
+			if ( isset( $_POST['__bswp_api_key'] ) ) {
+				$api_key_saved = $this->save_api_key( sanitize_text_field( wp_unslash( $_POST['__bswp_api_key'] ) ) );
+				if ( is_wp_error( $api_key_saved ) ) {
+					$this->error_msg = $api_key_saved->get_error_message();
+					add_action(
+						'admin_notices',
+						function () {
+							$class   = 'notice notice-error';
+							$message = $this->error_msg;
+							printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
+						}
+					);
+				}
 			}
 		}
 	}
