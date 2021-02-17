8282409b41ec0e0fc2673cec15786535bad92f3a
Brian Siklinski <bszyk.codes@gmail.com>
2021-02-17 11:47:07 -0600

    commented out unused class method
    this.toggleAddOn & this.toggle_addon don't exist within their classes

--- includes/AddOns/WooWIshlists/assets/index.js
+++ includes/AddOns/WooWIshlists/assets/index.js
@@ -3,25 +3,24 @@ import './styles.scss';
 export default class WooWishlist {
 
 	init($) {
-		this.$ = $;
+    this.$ = $;
 
-		// If no cloudsponge, return.
-		if ( typeof cloudsponge === 'undefined' || ! cloudsponge ) {
-			console.log('cloudsponge not defined, check API key');
-			return false;
-		}
+    // If no cloudsponge, return.
+    if (typeof cloudsponge === 'undefined' || !cloudsponge) {
+      console.log('cloudsponge not defined, check API key');
+      return false;
+    }
 
-		cloudsponge.init({
-			referrer: 'better-sharing-wp:woo-wishlists',
-			afterSubmitContacts: this.afterSelect
-		});
+    cloudsponge.init({
+      referrer: 'better-sharing-wp:woo-wishlists',
+      afterSubmitContacts: this.afterSelect,
+    });
 
-		const $body = $('body');
-		$body.on( 'click', '.add-from-address-book-init', this.clickInit );
-		$body.on( 'click', '.bswp-single-addon-status-toggle', this.toggleAddOn );
-		// doesn't apply to wishlist, nothing to copy.
-		// $body.on( 'click', '.bswp-copy', this.copyLink );
-	}
+		$body.on('click', '.add-from-address-book-init', this.clickInit);
+    // $body.on( 'click', '.bswp-single-addon-status-toggle', this.toggleAddOn );
+    // doesn't apply to wishlist, nothing to copy.
+    // $body.on( 'click', '.bswp-copy', this.copyLink );
+  }
 
 	/**
 	 * Init Address Book
