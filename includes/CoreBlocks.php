c0c18240d5fa008294fd5786a16f42160689a303
Brian Siklinski <bszyk.codes@gmail.com>
2021-03-01 23:17:13 -0600

    added shortcode w/ default attributes

--- includes/CoreBlocks.php
+++ includes/CoreBlocks.php
@@ -13,9 +13,59 @@ namespace BetterSharingWP;
 class CoreBlocks {
 
 	/**
+	 * Message to share with social share links.
+	 *
+	 * @var string $message Message.
+	 */
+	public $message;
+
+	/**
+	 * Default block attributes for shortcode output
+	 *
+	 * @var array  $block_attributes block attributes.
+	 */
+	public $block_attributes;
+
+	/**
 	 * Constructor
 	 */
-	public function __construct() {}
+	public function __construct() {
+		$this->message = 'Check out this link!';
+
+		$this->block_attributes = array(
+			'emailSubject'        => 'Sharing',
+			'emailMessage'        => 'What a great way to save! Click {{link}}',
+			'socialNetworks'      => array(
+				'twitter'  => array(
+					'visible'   => true,
+					'name'      => 'Twitter',
+					'icon'      => 'twitter',
+					'message'   => $this->message,
+					'intentUrl' => 'https://www.twitter.com/intent/tweet?url={{permalink}}&text=' . $this->message,
+				),
+				'facebook' => array(
+					'visible'   => true,
+					'name'      => 'Facebook',
+					'icon'      => 'facebook',
+					'message'   => 'No custom message available',
+					'intentUrl' => 'https://www.facebook.com/sharer/sharer.php?&u={{permalink}}',
+				),
+			),
+			'emailFormControl'    => 'readonly',
+			'referralLinkControl' => 'default',
+		);
+
+		add_action( 'init', array( $this, 'add_better_sharing_shortcode' ) );
+	}
+
+	/**
+	 * Register shortcode.
+	 *
+	 * @return void
+	 */
+	public function add_better_sharing_shortcode() {
+		add_shortcode( 'better-sharing', array( $this, 'render_better_sharing_output' ) );
+	}
 
 	/**
 	 * Public Scripts and Styles
@@ -23,7 +73,8 @@ class CoreBlocks {
 	 * @return void
 	 */
 	public function core_block_public_scripts() {
-		if ( has_block( 'cgb/block-ea-better-sharing' ) ) {
+		global $post;
+		if ( has_block( 'cgb/block-ea-better-sharing' ) || has_shortcode( $post->post_content, 'better-sharing' ) ) {
 			wp_enqueue_script(
 				'better-sharing-blocks-public',
 				BETTER_SHARING_URI . 'dist/blocks/public.bundle.js',
@@ -52,7 +103,7 @@ class CoreBlocks {
 			'cgb/block-ea-better-sharing',
 			array(
 				'editor_script'   => 'better-sharing-blocks',
-				'render_callback' => array( $this, 'render_block' ),
+				'render_callback' => array( $this, 'render_better_sharing_output' ),
 				// default attributes if not contained within $block_attributes.
 				'attributes'      => array(
 					'emailFormControl'    => array(
@@ -69,6 +120,17 @@ class CoreBlocks {
 	}
 
 	/**
+	 * Create intent url for shortcode.
+	 *
+	 * @param string $intent_url Social sharing intent URL w/o permalink.
+	 *
+	 * @return string
+	 */
+	public function create_intent_url( $intent_url ) {
+		return str_replace( '{{permalink}}', get_permalink(), $intent_url );
+	}
+
+	/**
 	 * Render Block
 	 *
 	 * @param array  $block_attributes block attributes.
@@ -76,12 +138,17 @@ class CoreBlocks {
 	 *
 	 * @return string
 	 */
-	public function render_block( $block_attributes, $content ) {
+	public function render_better_sharing_output( $block_attributes, $content ) {
+		// use default attributes for shortcode.
+		if ( '' === $block_attributes ) {
+			$block_attributes = $this->block_attributes;
+		}
+
 		$social_networks = $block_attributes['socialNetworks'];
-		$referral_link   = esc_url_raw( $block_attributes['referralLink'] );
+		$referral_link   = array_key_exists( 'referralLink', $block_attributes ) ? esc_url( $block_attributes['referralLink'] ) : false;
 		$email_control   = sanitize_text_field( $block_attributes['emailFormControl'] );
 
-			// replace '{{link}}' with actual link.
+		// replace '{{link}}' with actual link.
 		$email_content = sanitize_text_field( $block_attributes['emailMessage'] );
 		$email_content = str_replace( '{{link}}', $referral_link, $email_content );
 
@@ -109,7 +176,7 @@ class CoreBlocks {
 				$icon       = $network['icon'];
 				$intent_url = $network['intentUrl'];
 
-				echo '<a key="' . esc_attr( $key ) . '" href="' . esc_url_raw( $intent_url ) . '" target="_blank" ref="noopener noreferrer">';
+				echo '<a key="' . esc_attr( $key ) . '" href="' . esc_url( $this->create_intent_url( $intent_url ) ) . '" target="_blank" ref="noopener noreferrer">';
 				echo '<button class="components-button is-secondary is-small has-text has-icon">';
 				echo '<span class="dashicons dashicons-' . esc_attr( $icon ) . '"></span>&nbsp;';
 				echo esc_html( $name );
@@ -125,7 +192,7 @@ class CoreBlocks {
 		?>
 		<div class='referral-link'>
 			<label for='referral-link'>Your referral link</label>
-			<input type='text' id='referral-link' value='<?php echo esc_attr( $referral_link ); ?>' readOnly>
+			<input type='text' id='referral-link' value='<?php echo esc_attr( $referral_link ? $referral_link : get_permalink() ); ?>' readOnly>
 			<button class="components-button is-secondary is-small has-text has-icon" icon='admin-page' id='referral-btn-copy'>
 				Copy Link
 			</button>
