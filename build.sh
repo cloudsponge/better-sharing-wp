0955f298094a065b4d47eb7cd9911f58e47faaea
Graeme Rouse <graeme@cloudsponge.com>
2021-03-15 12:35:07 -0700

    logging to stdout and a detailed build log

--- build.sh
+++ build.sh
@@ -4,50 +4,54 @@ echo "WordPress Plugin Builder for Better Sharing for WP" &&
 echo "======================================================" &&
 echo "" &&
 
+me=`basename "$0"`
+
+log() {
+  echo "[$me] $1" | tee -a $buildLog
+}
+
 #Ask & Store Version
 read -r -p "Version Number: " versionNumber
 
 #Remove Build Directory?
-read -r -p "Remove plugin build directory after compression? (y/N) " removeDir
+read -r -p "Remove plugin build directory after compression? (y/N): " removeDir
 
 # push tags to gh?
-read -r -p "Push build tag to Github? (y/N) " prodBuild
+read -r -p "Push build tag to Github? (y/N): " prodBuild
 
 buildDir='build'
 mkdir -p "$buildDir"
 buildLog="$buildDir/build-$versionNumber.txt"
 
-echo "Building $versionNumber..."
 echo "output will go to $buildLog"
 
 # Create new branch
-echo "Checking out build"
+log "Creating a branch for the build"
 git checkout -b build-"$versionNumber" >> $buildLog 2>&1
 
 # Run composer
 # we use it as a way to do PSR-4 namespacing
 # remove the composer lock file, we don't require it because we don't have any external packages at this time.
-echo "Running composer"
+log "Running composer"
 composer install >> $buildLog 2>&1
 rm composer.lock >> $buildLog 2>&1
 
 # Run Build
-echo "Building version $versionNumber";
-npm run build >> $buildLog 2>&1;
-
+log "Building version $versionNumber"
+npm run build >> $buildLog 2>&1
 
 # Tag, if this is a prod build
 case $prodBuild in
 [Yy]*)
   #Create tag
-  echo "Creating Tag"
+  log "Creating Tag"
   git tag -a v"$versionNumber" -m "new version: $versionNumber" >> $buildLog 2>&1
 
-  echo "Pushing to GitHub"
+  log "Pushing to GitHub"
   git push --tags origin >> $buildLog 2>&1
   ;;
 *) 
-  echo "Not tagging this release, you indicated that it was not a production build."
+  log "Not tagging this release, you indicated that it was not a production build."
   ;;
 esac
 
@@ -55,39 +59,39 @@ esac
 STR="better-sharing-$versionNumber"
 mkdir "$STR" >> $buildLog 2>&1
 
-echo "Copying Admin Assets"
+log "Copying Admin Assets"
 cp -r ./admin-assets "./$STR/admin-assets" >> $buildLog 2>&1
 
-echo "Copying dist"
+log "Copying dist"
 cp -r ./dist "./$STR/dist" >> $buildLog 2>&1
 
-echo "Copying vendor dir"
+log "Copying vendor dir"
 cp -r ./vendor "./$STR/vendor" >> $buildLog 2>&1
 
-echo "Copying includes"
+log "Copying includes"
 cp -r ./includes "./$STR/includes" >> $buildLog 2>&1
 
-echo "Copying public"
+log "Copying public"
 cp -r ./public "./$STR/public" >> $buildLog 2>&1
 
-echo "Copying core files"
+log "Copying core files"
 cp ./BetterSharingWP.php "./$STR/BetterSharingWP.php" >> $buildLog 2>&1
 
-echo "Copying Readmes"
+log "Copying Readmes"
 cp ./README.md "./$STR/README.md" >> $buildLog 2>&1
 cp ./readme.txt "./$STR/readme.txt" >> $buildLog 2>&1
 
-echo "Compressing"
+log "Compressing"
 zip -r "$STR.zip" "./$STR" >> $buildLog 2>&1
 
 case $removeDir in
 [Yy]* )
-  echo "Cleanup..."
+  log "Cleanup build folder..."
   rm -rf "./$STR" >> $buildLog 2>&1
 ;;
 esac
 
-echo "Thank You"
+log "Thank You"
 
 # we're good!
 exit 0
